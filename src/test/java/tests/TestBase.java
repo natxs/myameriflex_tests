d692d3fdac5a5a58ac21ff53939f2ccf816d449f
nvatstx <natalia.vats@gmail.com>
2020-09-06 00:03:01 -0500

    Added TestBase

---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,36 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import io.qameta.allure.Step;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+
+import static com.codeborne.selenide.WebDriverRunner.closeWebDriver;
+import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
+import static helpers.AttachmentsHelper.*;
+import static org.openqa.selenium.logging.LogType.BROWSER;
+
+public class TestBase {
+    @BeforeAll
+    @Step("Tests setup")
+    public static void beforeAll() {
+        addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
+        Configuration.headless = true;
+    }
+
+    @AfterEach
+    @Step("Attachments")
+    public void afterEach(){
+        attachScreenshot("Last screenshot");
+        attachPageSource();
+        attachAsText("getBrowserConsoleLogs console logs", getBrowserConsoleLogs());
+
+        closeWebDriver();
+    }
+
+    public static String getBrowserConsoleLogs() {
+        return String.join("\n", Selenide.getWebDriverLogs(BROWSER));
+    }
+}
